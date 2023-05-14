c87b76e501775b75d7feb1c9873d397b561185c0
Mukul Mantosh <mukulmantosh@yahoo.com>
2023-05-14 12:33:18 +0530

    Feat: - Initial Commit

---
+++ deck.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+// create a new type of 'deck'
+// which is a slice of strings
+
+type deck []string
+
+//type mobile []string
+
+//func (num mobile) mobileNumber(extension string) string {
+//	return extension + "1234567890"
+//}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	data := []string(d)
+	return strings.Join(data, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func (d deck) newDeckFromFile(filename string) deck {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	result := strings.Split(string(data), ",")
+	return deck(result)
+
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	for i, _ := range d {
+		newPosition := random.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hi There !")
+	//var number = mobile{}
+	//fmt.Println(number.mobileNumber("+91"))
+
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+	//err := cards.saveToFile("my_cards.txt")
+	//if err != nil {
+	//	return
+	//}
+
+	//readFile := cards.newDeckFromFile("my_cards.txt")
+	//fmt.Println(readFile)
+
+	//cards.print()
+	//hand, remainingCards := deal(cards, 5)
+	//hand.print()
+	//fmt.Println("    ")
+	//remainingCards.print()
+	//fmt.Println(cards.toString())
+
+	//greeting := "Hi there"
+	//fmt.Println([]byte(greeting))
+
 }
